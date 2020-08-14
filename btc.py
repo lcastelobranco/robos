28da7c695e85bae757ce0a63b03cea285533db20
lcastelobranco <castelobrancoluiz@gmail.com>
2020-08-14 19:01:50 -0300

    coluna isin no btc agora é sempre vazia (evita q o robo fique dando erro em empesas novas 'IPO')

--- btc.py
+++ btc.py
@@ -31,7 +31,7 @@ try:
         h = "http://www.b3.com.br/pt_br/produtos-e-servicos/emprestimo-de-ativos/renda-variavel/posicoes-em-aberto/renda-variavel-8AE490C9701B5B35017039842ACE1F91.htm?data="+data+"&f=0"
         r = requests.post(h,params={"data": data})
         soup = BeautifulSoup(r.content, 'html.parser')
-        isin = pd.read_sql("select ticker,isin from btc",conn)
+        #isin = pd.read_sql("select ticker,isin from btc",conn)
         for t in range(len(soup.tbody.find_all("td")[0::5])):
             str_sql = "INSERT INTO btc (data,ticker,nome,tipo_papel,isin,qtd_total,avg_price,fator_cot,volume) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             
@@ -39,7 +39,8 @@ try:
             lista.append(list(map(lambda x: x.text.strip().upper(),soup.tbody.find_all("td")[0::5]))[t])
             lista.append(list(map(lambda x: x.text.strip().upper().replace("B3 S.A. ¿ BRASI","B3 S.A. - BRASI"),soup.tbody.find_all("td")[1::5]))[t])
             lista.append(list(map(lambda x: x.text.strip().upper(),soup.tbody.find_all("td")[2::5]))[t])
-            lista.append(isin[isin.ticker==lista[1]].values[0][1])
+            #lista.append(isin[isin.ticker==lista[1]].values[0][1])
+            lista.append('')
             lista.append(list(map(lambda x: x.text.strip().replace(".",'').replace(",","."),soup.tbody.find_all("td")[3::5]))[t])
             lista.append('')
             lista.append('1')
